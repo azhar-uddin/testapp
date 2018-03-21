3fdea8396be05dc987db82704785ad483c2edfac
Md Azharuddin <mohammad.azhar2000@gmail.com>
2018-03-21 22:07:08 +0530

    shrt

--- shortestdistance.py
+++ shortestdistance.py
@@ -10,25 +10,6 @@ def print_board(board):
 
 def validate_input(row, column, inpt):
   while (row < 0 or row > 11) or (column < 0 or column > 11) or board[row][column] == "X":
-    print "Kindly Enter a valid Position"
-    take_input(inpt)
-    if inpt == "Source":
-      row, column = source_row, source_col
-    if inpt == "Destination":
-      row, column = destination_row, destination_col
-    if inpt == "Obstacles":
-      row, column = obstacle_row, obstacle_col 
-
-def take_input(input):
-  global source_row, source_col, destination_row, destination_col, obstacle_row, obstacle_col
-  print "Enter the {}".format(input)
-  if input == "Obstacles":
-    obstacle_row = int(raw_input("Obstacle Row: "))
-    obstacle_col = int(raw_input("Obstacle Col: "))
-    validate_input(obstacle_row, obstacle_col, input)
-  if input == "Source":
-    source_row= int(raw_input("Source Row: "))
-    source_col = int(raw_input("Source Col: "))
     validate_input(source_row, source_col, input)
   elif input == "Destination":
     destination_row = int(raw_input("Destination Row: "))
